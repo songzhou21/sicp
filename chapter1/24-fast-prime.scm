12710f80bc5e0c1a1846c88705cc7324e6a04c1b
Song Zhou <zhousong1993@gmail.com>
2019-04-16 18:57:17 +0800

    24

---
+++ chapter1/24-fast-prime.scm
@@ -0,0 +1,42 @@
+(load "22-timed-prime-test.scm")
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder 
+           (square (expmod base (/ exp 2) m))
+           m
+           ))
+        (else
+          (remainder 
+            (* base (expmod base (- exp 1) m) 
+               m
+               )
+            )
+          )
+        )
+  )
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+
+  (try-it (+ 1 (random (- n 1))))
+  )
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)
+        )
+  )
+
+(define (prime n)
+  (fast-prime? n)
+  )
+
+
+(search-for-primes 1000)
+(search-for-primes 10000)
+(search-for-primes 100000)
+(search-for-primes 1000000)
