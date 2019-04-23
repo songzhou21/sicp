02657e6cde038e72a623b77ced4ac5f969692e56
Song Zhou <zhousong1993@gmail.com>
2019-04-23 15:23:25 +0800

    2-1

---
+++ chapter2/1-make-rat.scm
@@ -0,0 +1,57 @@
+#lang sicp
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+
+;(define (make-rat n d)
+;  (cons n d))
+
+(define (make-rat n d)
+  (if (< d 0)
+      (cons (- n) (- d))
+      (cons n d)
+      ))
+
+
+(define (numer x)
+  (let ((g (gcd (car x) (cdr x))))
+    (/ (car x) g)))
+
+(define (denom x)
+  (let ((g (gcd (car x) (cdr x))))
+    (/ (cdr x) g)))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define one-third (make-rat 1 3))
+
+(print-rat (add-rat one-third one-third))
+
+
+(print-rat (make-rat 1 3))
+(print-rat (make-rat -1 -3))
+(print-rat (make-rat 1 -3))
+(print-rat (make-rat -1 3))
