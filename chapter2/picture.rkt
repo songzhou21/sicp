ce3b7b745e04a25d3b7d7a250627eca01c0f1c7d
Song Zhou <zhousong1993@gmail.com>
2019-04-28 18:37:36 +0800

    add 2.50

--- chapter2/picture.rkt
+++ chapter2/picture.rkt
@@ -210,4 +210,50 @@
 #;(painter-diamond unit-frame)
 #;(wave-painter unit-frame)
 
+;; Transformation
+(define (transform-painter painter origin corner1 corner2)
+  (lambda (frame)
+    (let ((m (frame-coord-map frame)))
+      (let ((new-origin (m origin)))
+        (painter
+         (make-frame new-origin
+                     (sub-vect (m corner1) new-origin)
+                     (sub-vect (m corner2) new-origin)))))))
 
+(define (flip-vert painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)    ; new origin
+                     (make-vect 1.0 1.0)    ; new end of edge1
+                     (make-vect 0.0 0.0)))  ; new end of edge2
+
+(define (rotate90 painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+  
+;; EXERCISE 2.50
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)
+                     ))
+
+(define (rotate180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)
+                     ))
+
+(define (rotate270 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)
+                     ))
+  
+#;((flip-horiz wave-painter) unit-frame)
+#;((rotate180 wave-painter) unit-frame)
+#;((rotate270 wave-painter) unit-frame)
