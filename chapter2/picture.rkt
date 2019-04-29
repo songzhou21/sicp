5d2c9a8cf5a99991658aaf80927bbefb48d59142
Song Zhou <zhousong1993@gmail.com>
2019-04-29 09:28:21 +0800

    add 2.51

--- chapter2/picture.rkt
+++ chapter2/picture.rkt
@@ -231,7 +231,23 @@
                      (make-vect 1.0 0.0)
                      (make-vect 1.0 1.0)
                      (make-vect 0.0 0.0)))
-  
+
+(define (beside painter1 painter2)
+  (let ((split-point (make-vect 0.5 0.0)))
+    (let ((paint-left
+           (transform-painter painter1
+                              (make-vect 0.0 0.0)
+                              split-point
+                              (make-vect 0.0 1.0)))
+          (paint-right
+           (transform-painter painter2
+                              split-point
+                              (make-vect 1.0 0.0)
+                              (make-vect 0.5 1.0))))
+      (lambda (frame)
+        (paint-left frame)
+        (paint-right frame)))))
+
 ;; EXERCISE 2.50
 (define (flip-horiz painter)
   (transform-painter painter
@@ -257,3 +273,34 @@
 #;((flip-horiz wave-painter) unit-frame)
 #;((rotate180 wave-painter) unit-frame)
 #;((rotate270 wave-painter) unit-frame)
+
+;; EXERCISE 2.51
+(define (below painter1 painter2)
+  (let ((paint-top (transform-painter
+                    painter2
+                    (make-vect 0.0 0.5)
+                    (make-vect 1.0 0.5)
+                    (make-vect 0.0 1.0)
+                    ))
+        (paint-bottom (transform-painter
+                       painter1
+                       (make-vect 0.0 0.0)
+                       (make-vect 1.0 0.0)
+                       (make-vect 0.0 0.5)
+                       )))
+    (lambda (frame)
+      (paint-top frame)
+      (paint-bottom frame)))
+  )
+
+(define (below2 painter1 painter2)
+  (lambda (frame)
+    ((rotate90
+      (beside (rotate270 painter1)
+              (rotate270 painter2)))
+     frame)
+    ))
+
+#;((beside wave-painter wave-painter) unit-frame)
+#;((below wave-painter wave-painter) unit-frame)
+#;((below2 wave-painter wave-painter) unit-frame)
