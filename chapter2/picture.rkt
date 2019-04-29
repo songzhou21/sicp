7d078ff170006f1e2b1c028724bf95151b4e267e
Song Zhou <zhousong1993@gmail.com>
2019-04-29 10:24:16 +0800

    add square-limit

--- chapter2/picture.rkt
+++ chapter2/picture.rkt
@@ -70,13 +70,6 @@
 (define (start-segment segment)
   (car segment)
   )
-(define (below2 painter1 painter2)
-  (lambda (frame)
-    ((rotate90
-      (beside (rotate270 painter1)
-              (rotate270 painter2)))
-     frame)
-    ))
 (define (end-segment segment)
   (cadr segment)
   )
@@ -309,4 +302,42 @@
 
 #;((beside wave-painter wave-painter) unit-frame)
 #;((below wave-painter wave-painter) unit-frame)
-((below2 wave-painter wave-painter) unit-frame)
+#;((below2 wave-painter wave-painter) unit-frame)
+
+;; EXERCISE 2.52
+(define (up-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (up-split painter (- n 1))))
+        (below painter (beside smaller smaller))
+      )
+  ))
+
+(define (right-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (right-split painter (- n 1))))
+        (beside painter (below smaller smaller)))))
+
+
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left (beside up up))
+              (bottom-right (below right right))
+              (corner (corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
+
+
+(define (square-limit painter n)
+  (let ((quarter (corner-split painter n)))
+    (let ((half (beside (flip-horiz quarter) quarter)))
+      (below (flip-vert half) half))))
+
+
+#;((right-split wave-painter 1) unit-frame)
+#;((corner-split wave-painter 1) unit-frame)
+#;((square-limit wave-painter 4) unit-frame)
