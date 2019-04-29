d187a8eb825742398f259a0b749ec598eec819e9
Song Zhou <zhousong1993@gmail.com>
2019-04-29 11:30:15 +0800

    2.5.2

--- chapter2/picture.rkt
+++ chapter2/picture.rkt
@@ -341,3 +341,88 @@
 #;((right-split wave-painter 1) unit-frame)
 #;((corner-split wave-painter 1) unit-frame)
 #;((square-limit wave-painter 4) unit-frame)
+
+;; a
+
+(define wave-painter2
+  (segments->painter
+   (list
+    (make-segment (make-vect 0.5 0.4) ;;; leg triangle
+                  (make-vect 0.6 0))
+    (make-segment (make-vect 0.5 0.4)
+                  (make-vect 0.4 0))
+    (make-segment (make-vect 0.3 0)
+                  (make-vect 0.35 0.4))
+    (make-segment (make-vect 0.35 0.4)
+                  (make-vect 0.3 0.7))
+    (make-segment (make-vect 0.3 0.7)
+                  (make-vect 0.2 0.6))
+    (make-segment (make-vect 0.2 0.6)
+                  (make-vect 0 0.8))
+    (make-segment (make-vect 0 0.9)
+                  (make-vect 0.2 0.7))
+    (make-segment (make-vect 0.2 0.7)
+                  (make-vect 0.3 0.75))
+    (make-segment (make-vect 0.3 0.75)
+                  (make-vect 0.4 0.75))
+    (make-segment (make-vect 0.4 0.75)
+                  (make-vect 0.35 0.9))
+    (make-segment (make-vect 0.35 0.9)
+                  (make-vect 0.4 1))
+    (make-segment (make-vect 0.5 1)
+                  (make-vect 0.55 0.9))
+    (make-segment (make-vect 0.55 0.9)
+                  (make-vect 0.5 0.75))
+    (make-segment (make-vect 0.5 0.75)
+                  (make-vect 0.6 0.75))
+    (make-segment (make-vect 0.6 0.75)
+                  (make-vect 1 0.45))
+    (make-segment (make-vect 1 0.3)
+                  (make-vect 0.6 0.5))
+    (make-segment (make-vect 0.6 0.5)
+                  (make-vect 0.7 0))
+    (make-segment (make-vect 0.0 0.0)
+                  (make-vect 1.0 1.0))
+ )))
+
+#;(wave-painter2 unit-frame)
+
+
+;; b
+
+(define (corner-split2 painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left up)
+              (bottom-right right)
+              (corner (corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
+
+(define (square-limit2 painter n)
+  (let ((quarter (corner-split2 painter n)))
+    (let ((half (beside (flip-horiz quarter) quarter)))
+      (below (flip-vert half) half))))
+
+#;((square-limit2 wave-painter 4) unit-frame)
+
+;; c
+(define (square-of-four tl tr bl br)
+  (lambda (painter)
+    (let ((top (beside (tl painter) (tr painter)))
+          (bottom (beside (bl painter) (br painter))))
+      (below bottom top))))
+
+
+
+(define (square-limit3 painter n)
+  (let ((combine4 (square-of-four  flip-vert rotate180 identity  flip-horiz)))
+
+        (combine4 (corner-split painter n))
+        ))
+
+#;(((square-of-four flip-horiz identity rotate180 flip-vert) wave-painter) unit-frame)
+((square-limit3 wave-painter 4) unit-frame)
+   
