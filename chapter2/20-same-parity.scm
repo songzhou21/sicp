beca2584d510a410d80e48cdbdb29dc00ce30f15
Song Zhou <zhousong1993@gmail.com>
2019-05-01 23:50:27 +0800

    2.20

---
+++ chapter2/20-same-parity.scm
@@ -0,0 +1,22 @@
+#lang sicp
+
+(define (same-property first)
+  (lambda (n)
+    (if (odd? first)
+        (odd? n)
+        (even? n))))
+
+(define (same-parity-iter first remain)
+  (cond ((null? remain) nil)
+        (((same-property first)
+          (car remain))
+         (cons (car remain)
+               (same-parity-iter first (cdr remain))))
+        (else (same-parity-iter first(cdr remain)))))
+
+(define (same-parity first . remain)
+  (cons first
+        (same-parity-iter first remain)))
+
+(same-parity 1 2 3 4 5 6 7)
+(same-parity 2 3 4 5 6 7 8)
