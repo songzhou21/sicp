1f2831d6153bac63779aa7b1ffb1b3133f38c12d
Song Zhou <zhousong1993@gmail.com>
2019-05-02 20:41:29 +0800

    2.28

---
+++ chapter2/28-fringe.scm
@@ -0,0 +1,15 @@
+#lang sicp
+
+(define (fringe tree)
+  (cond ((null? tree)
+         `())
+        ((not (pair? tree))
+         (list tree))
+        (else
+         (append (fringe (car tree))
+                 (fringe (cdr tree)))
+         )))
+
+(define x (list (list 1 2) (list 3 4)))
+(fringe x)
+(fringe (list x x))
