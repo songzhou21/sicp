e4ea686714d32d54717b66cdee4e347819babd72
SongZhou <zhousong1993@gmail.com>
2019-05-05 22:00:29 +0800

    2.30

---
+++ chapter2/30-map.scm
@@ -0,0 +1,33 @@
+#lang sicp
+
+(define (square x)
+  (* x x))
+
+(define (square-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (square tree))
+        (else (cons (square-tree (car tree))
+                    (square-tree (cdr tree))))))
+
+(define (scale-tree tree factor)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (scale-tree sub-tree factor)
+             (* sub-tree factor)))
+       tree))
+
+
+(define (square-tree-map tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (square-tree-map sub-tree)
+             (square sub-tree)))
+       tree))
+
+; test
+(define tree-data (list 1
+                        (list 2 (list 3 4) 5)
+                        (list 6 7)))
+
+(square-tree tree-data)
+(square-tree-map tree-data)
