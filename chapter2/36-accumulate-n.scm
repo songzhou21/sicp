380001be97d07fce4c32a170722475e9f0883b2f
Song Zhou <zhousong1993@gmail.com>
2019-05-10 10:26:02 +0800

    2.36

--- chapter2/36-accumulate-n.scm
+++ chapter2/36-accumulate-n.scm
@@ -1,9 +1,25 @@
 #lang sicp
 
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+
 (define (accumulate-n op init seqs)
   (if (null? (car seqs))
       nil
-      (cons (accumulate op init ??FILL-THIS-IN??)
-            (accumulate-n op init ??FILL-THIS-IN??))))
+      (cons (accumulate op init (map car
+                                     seqs))
+            (accumulate-n op init (map cdr
+                                       seqs)))))
 
-;: (accumulate-n + 0 s)
+; test
+(define data
+  (list (list 1 2 3)
+        (list 4 5 6)
+        (list 7 8 9)
+        (list 10 11 12)))
+
+(accumulate-n + 0 data)
