7115383e1897000746d204184c3ac84549c16ff1
Song Zhou <zhousong1993@gmail.com>
2019-05-12 22:50:24 +0800

    chagne to quick one

--- chapter2/42-eight-queens.scm
+++ chapter2/42-eight-queens.scm
@@ -24,20 +24,18 @@
 
 ; solution
 (define (queens board-size)
-  (define (queen-cols k)  
+  (define (queen-cols k)
     (if (= k 0)
         (list empty-board)
         (filter
          (lambda (positions) (safe? k positions))
-	 ;; next expression changed
          (flatmap
-	  (lambda (new-row)
-	    (map (lambda (rest-of-queens)
-		   (adjoin-position new-row k rest-of-queens))
-		 (queen-cols (- k 1))))
-	  (enumerate-interval 1 board-size)))))
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position new-row k rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
   (queen-cols board-size))
-
 
 (define empty-board `())
 
