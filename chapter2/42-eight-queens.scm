8e45d3d6c84d94c799ec429019c0834866916900
Song Zhou <zhousong1993@gmail.com>
2019-05-12 22:48:01 +0800

    2.42

---
+++ chapter2/42-eight-queens.scm
@@ -0,0 +1,71 @@
+#lang sicp
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+; solution
+(define (queens board-size)
+  (define (queen-cols k)  
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+	 ;; next expression changed
+         (flatmap
+	  (lambda (new-row)
+	    (map (lambda (rest-of-queens)
+		   (adjoin-position new-row k rest-of-queens))
+		 (queen-cols (- k 1))))
+	  (enumerate-interval 1 board-size)))))
+  (queen-cols board-size))
+
+
+(define empty-board `())
+
+(define (adjoin-position new-row k rest-of-queens)
+  (cons new-row rest-of-queens))
+
+(define (safe? k position)
+  (iter-check (car position)
+              (cdr position)
+              1))
+
+(define (iter-check row-of-new-queen rest-of-queens i)
+  (if (null? rest-of-queens)
+      #t
+      (let ((row-of-current-queen (car rest-of-queens)))
+        (if (or (= row-of-new-queen row-of-current-queen)
+                (= row-of-new-queen (+ i row-of-current-queen))
+                (= row-of-new-queen (- row-of-current-queen i)))
+            #f
+            (iter-check row-of-new-queen
+                        (cdr rest-of-queens)
+                        (+ i 1))))))
+  
+  
+
+(queens 8)
+
+
+ 
+
+
