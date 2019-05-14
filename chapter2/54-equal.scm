c4376d8c2e66da7192296500c9431764846174e6
Song Zhou <zhousong1993@gmail.com>
2019-05-14 09:26:20 +0800

    2.54

---
+++ chapter2/54-equal.scm
@@ -0,0 +1,34 @@
+#lang sicp
+
+(define (equal? x y)
+  (cond ((and (symbol? x) (symbol? y))
+         (symbol-equal? x y))
+        ((and (list? x) (list? y))
+         (list-equal? x y))
+        (else
+         false)))
+
+(define (symbol-equal? x y)
+  (eq? x y))
+
+(define (list-equal? x y)
+  (cond ((and (null? x) (null? y))
+         true)
+        ((or (null? x) (null? y))
+         false)
+        ((equal? (car x) (car y))
+         (equal? (cdr x) (cdr y)))
+        (else 
+         false)))
+
+; test
+
+(equal? '(this is a list)
+        '(this is a list))
+
+
+(equal? '(this (is a) list)
+        '(this (is a) list))
+
+(equal? '(this is a list)
+        '(this (is a) list))
