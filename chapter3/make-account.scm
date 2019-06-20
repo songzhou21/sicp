bb5eb8a9cd8aa2e7a520f9979663f9aaa67e3932
Song Zhou <zhousong1993@gmail.com>
2019-06-20 10:25:35 +0800

    3.4

--- chapter3/make-account.scm
+++ chapter3/make-account.scm
@@ -1,29 +1,49 @@
 (define (make-account balance password)
-  (define (withdraw amount)
-    (if (>= balance amount)
-        (begin (set! balance 
-                     (- balance amount))
-               balance)
-        "Insufficient funds"))
+  (let ((count 0))
 
-  (define (deposit amount)
-    (set! balance (+ balance amount))
-    balance)
+      (define (withdraw amount)
+        (if (>= balance amount)
+            (begin (set! balance 
+                         (- balance amount))
+                   balance)
+            "Insufficient funds"))
 
-  (define (dispatch test-password m)
-    (if (not (eq? password test-password))
-        (error "Incorrect password"))
+      (define (deposit amount)
+        (set! balance (+ balance amount))
+        balance)
 
-    (cond ((eq? m 'withdraw) withdraw)
-          ((eq? m 'deposit) deposit)
-          (else (error "Unknown request: 
-                 MAKE-ACCOUNT" m))))
+      (define (call-the-cops)
+        (error "cops are comming"))
 
-  dispatch)
+      (define (display-wrong-password-message useless-arg)
+        (newline)
+        (display "Incorrect password"))
+
+      (define (dispatch test-password m)
+        (if (eq? password test-password)
+            (begin
+              (set! count 0)
+              (cond ((eq? m 'withdraw) withdraw)
+                    ((eq? m 'deposit) deposit)
+                    (else (error "Unknown request: 
+                                 MAKE-ACCOUNT" m))))
+            (begin 
+              (set! count (+ count 1))
+              (if (>= count 7)
+                  (call-the-cops)
+                  display-wrong-password-message))))
+
+      dispatch))
 
 ;; test
 (define acc
   (make-account 100 'secret-password))
 
 ((acc 'secret-password 'withdraw) 40)
+((acc 'some-other-password 'deposit) 50)
+((acc 'some-other-password 'deposit) 50)
+((acc 'some-other-password 'deposit) 50)
+((acc 'some-other-password 'deposit) 50)
+((acc 'some-other-password 'deposit) 50)
+((acc 'some-other-password 'deposit) 50)
 ((acc 'some-other-password 'deposit) 50)
