40cbfad4a03362121749423f19af37e38a5a6c72
Song Zhou <zhousong1993@gmail.com>
2019-06-24 11:09:54 +0800

    3.10

---
+++ chapter3/3.10.scm
@@ -0,0 +1,21 @@
+;(define (make-withdraw initial-amount)
+;  (let ((balance initial-amount))
+;    (lambda (amount)
+;      (if (>= balance amount)
+;          (begin (set! balance (- balance amount))
+;                 balance)
+;          "Insufficient funds"))))
+
+
+(define (make-withdraw initial-amount)
+  ((lambda (balance)
+     (lambda (amount)
+       (if (>= balance amount)
+           (begin (set! balance (- balance amount))
+                  balance)
+           "Insufficient funds")))
+   initial-amount))
+
+(define x (make-withdraw 100))
+(x 0)
+(x 50)
