de4d1c424ae38594dff9afdcf64cd551ab150b69
Song Zhou <zhousong1993@gmail.com>
2019-06-26 17:56:03 +0800

    3.21

---
+++ chapter3/3.21.scm
@@ -0,0 +1,20 @@
+(load "queue.scm")
+
+(define (print-queue q)
+  (front-ptr q))
+
+(define q1 (make-queue))
+(print-queue q1)
+
+(insert-queue! q1 'a)
+(print-queue q1)
+
+(insert-queue! q1 'b)
+(print-queue q1)
+
+(delete-queue! q1)
+(print-queue q1)
+
+
+
+
