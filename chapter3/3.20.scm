aed74b4bfc23636636acd9b3e7ee3a9a6ef94c18
Song Zhou <zhousong1993@gmail.com>
2019-07-04 09:07:22 +0800

    add 3.20 svg

---
+++ chapter3/3.20.scm
@@ -0,0 +1,28 @@
+(define (cons x y)
+  (define (set-x! v) (set! x v))
+  (define (set-y! v) (set! y v))
+  (define (dispatch m)
+    (cond ((eq? m 'car) x)
+          ((eq? m 'cdr) y)
+          ((eq? m 'set-car!) set-x!)
+          ((eq? m 'set-cdr!) set-y!)
+
+
+(define (car z) (z 'car))
+(define (cdr z) (z 'cdr))
+
+(define (set-car! z new-value)
+  ((z 'set-car!) new-value)
+  z)
+
+(define (set-cdr! z new-value)
+  ((z 'set-cdr!) new-value)
+  z)
+
+;; test
+
+(define x (cons 1 2))
+x
+(define z (cons x x))
+(set-car! (cdr z) 17)
+(car x)
