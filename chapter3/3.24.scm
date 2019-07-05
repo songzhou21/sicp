6c3d7391bd959bd489d8baa268ecc4a95178ea91
Song Zhou <zhousong1993@gmail.com>
2019-07-05 12:42:53 +0800

    3.24

---
+++ chapter3/3.24.scm
@@ -0,0 +1,51 @@
+;; local tables
+(define (make-table same-key?)
+  (let ((local-table (list '*table*)))
+
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (cdr record)
+                  false))
+            false)))
+
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1
+                                  (cons key-2 value))
+                            (cdr local-table)))))
+      'ok)    
+
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown operation -- TABLE" m))))
+
+    (define (assoc key records)
+      (cond ((null? records) false)
+            ((same-key? key (caar records)) (car records))
+            (else (assoc key (cdr records)))))
+
+    dispatch))
+
+;; test
+(define number-table (make-table =))
+
+((number-table 'insert-proc!) 10086 10086 'hello-moto)
+((number-table 'lookup-proc) 10086 10086)
+
+
+(define symbol-table (make-table eq?))
+
+((symbol-table 'insert-proc!) 'peter 'age 25)
+((symbol-table 'lookup-proc) 'peter 'age)
